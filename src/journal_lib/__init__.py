dc1a246bb34ecbf96979a332e68def8e465e776b
jakobst1n <jakob.stendahl@outlook.com>
2023-12-26 23:17:49 +0100

    Fix import

--- src/journal_lib/__init__.py
+++ src/journal_lib/__init__.py
@@ -1,3 +1,4 @@
 __version__ = "1.0.0"
 
 from .utils import journal_from_str, journal_from_file
+from .dataclasses import JournalEntryTransaction, JournalEntry, JournalAccountDef, JournalCommodityDef, Journal, set_force_nocolor, DATE_FORMAT
